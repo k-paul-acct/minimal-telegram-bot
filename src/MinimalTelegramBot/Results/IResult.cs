04d9b197516d158541afaa03baa5f0068e0c3f87
Pavel Kulakov <me@k-pavel.xyz>
2024-09-17 15:47:31 +0300

    Results feature documentation

--- src/MinimalTelegramBot/Results/IResult.cs
+++ src/MinimalTelegramBot/Results/IResult.cs
@@ -1,6 +1,14 @@
 namespace MinimalTelegramBot.Results;
 
+/// <summary>
+/// Represents the interface for some executable result as an action on an update.
+/// </summary>
 public interface IResult
 {
+    /// <summary>
+    /// Executes the result asynchronously.
+    /// </summary>
+    /// <param name="context">The bot request context.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
     Task ExecuteAsync(BotRequestContext context);
 }
