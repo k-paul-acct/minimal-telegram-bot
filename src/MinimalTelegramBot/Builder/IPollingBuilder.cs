9bf744b62e283ce73fa3238b8162b197063fb1e7
Pavel Kulakov <me@k-pavel.xyz>
2024-09-29 00:41:26 +0300

    Builder docs

--- src/MinimalTelegramBot/Builder/IPollingBuilder.cs
+++ src/MinimalTelegramBot/Builder/IPollingBuilder.cs
@@ -2,8 +2,23 @@ using Microsoft.AspNetCore.Builder;
 
 namespace MinimalTelegramBot.Builder;
 
+/// <summary>
+///     Interface for building polling configuration.
+/// </summary>
 public interface IPollingBuilder
 {
+    /// <summary>
+    ///     Configures the application to serve static files.
+    /// </summary>
+    /// <param name="url">The base URL to be used as the base URL for the media URLs sent by the bot.</param>
+    /// <returns>The current instance of <see cref="IPollingBuilder"/>.</returns>
     IPollingBuilder UseStaticFiles(string url);
+
+    /// <summary>
+    ///     Configures the application to serve static files.
+    /// </summary>
+    /// <param name="url">The base URL to be used as the base URL for the media URLs sent by the bot.</param>
+    /// <param name="options">The options for serving static files.</param>
+    /// <returns>The current instance of <see cref="IPollingBuilder"/>.</returns>
     IPollingBuilder UseStaticFiles(string url, StaticFileOptions options);
 }
--- src/MinimalTelegramBot/Builder/IWebhookBuilder.cs
+++ src/MinimalTelegramBot/Builder/IWebhookBuilder.cs
@@ -2,10 +2,34 @@ using Microsoft.AspNetCore.Builder;
 
 namespace MinimalTelegramBot.Builder;
 
+/// <summary>
+///     Interface for building webhook configuration.
+/// </summary>
 public interface IWebhookBuilder
 {
+    /// <summary>
+    ///     Enables the direct webhook response to incoming updates.
+    /// </summary>
+    /// <returns>The current instance of <see cref="IWebhookBuilder"/>.</returns>
     IWebhookBuilder EnableWebhookResponse();
+
+    /// <summary>
+    ///     Configures the webhook to listen on the specified path.
+    /// </summary>
+    /// <param name="path">The path on which the webhook will listen.</param>
+    /// <returns>The current instance of <see cref="IWebhookBuilder"/>.</returns>
     IWebhookBuilder ListenOnPath(string path);
+
+    /// <summary>
+    ///     Configures the webhook to be deleted when the application shuts down.
+    /// </summary>
+    /// <returns>The current instance of <see cref="IWebhookBuilder"/>.</returns>
     IWebhookBuilder DeleteWebhookOnShutdown();
+
+    /// <summary>
+    ///     Configures the webhook to use the custom web application.
+    /// </summary>
+    /// <param name="app">The web application to use for the webhook.</param>
+    /// <returns>The current instance of <see cref="IWebhookBuilder"/>.</returns>
     IWebhookBuilder UseWebApplication(WebApplication app);
 }
