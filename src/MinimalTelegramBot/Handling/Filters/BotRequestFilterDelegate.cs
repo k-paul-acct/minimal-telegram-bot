131f3c0751617398e424f1b297a7f0d57ac9b328
Pavel Kulakov <me@k-pavel.xyz>
2024-09-28 22:55:47 +0300

    Full filters docs

--- src/MinimalTelegramBot/Handling/Filters/BotRequestFilterDelegate.cs
+++ src/MinimalTelegramBot/Handling/Filters/BotRequestFilterDelegate.cs
@@ -1,3 +1,6 @@
 namespace MinimalTelegramBot.Handling.Filters;
 
+/// <summary>
+///     Represents a method that can be used to filter a bot request.
+/// </summary>
 public delegate ValueTask<IResult> BotRequestFilterDelegate(BotRequestFilterContext context);
