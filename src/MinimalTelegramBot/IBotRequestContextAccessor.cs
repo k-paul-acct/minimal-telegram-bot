05aa94efc897dc60d4a3a3145e01aeabde459838
Pavel Kulakov <me@k-pavel.xyz>
2024-09-29 00:04:07 +0300

    Generals docs

--- src/MinimalTelegramBot/IBotRequestContextAccessor.cs
+++ src/MinimalTelegramBot/IBotRequestContextAccessor.cs
@@ -1,6 +1,12 @@
 namespace MinimalTelegramBot;
 
+/// <summary>
+///     Provides access to the current <see cref="BotRequestContext"/>.
+/// </summary>
 public interface IBotRequestContextAccessor
 {
+    /// <summary>
+    ///     Gets or sets the current <see cref="BotRequestContext"/>.
+    /// </summary>
     BotRequestContext? BotRequestContext { get; set; }
 }
