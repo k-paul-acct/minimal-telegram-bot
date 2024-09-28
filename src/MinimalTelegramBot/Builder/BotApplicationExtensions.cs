2890d0af83210df5baf03bd25e363b83af273265
Pavel Kulakov <me@k-pavel.xyz>
2024-09-29 00:49:17 +0300

    Builder extensions docs

--- src/MinimalTelegramBot/Builder/BotApplicationExtensions.cs
+++ src/MinimalTelegramBot/Builder/BotApplicationExtensions.cs
@@ -4,6 +4,13 @@ namespace MinimalTelegramBot.Builder;
 
 public static class BotApplicationExtensions
 {
+    /// <summary>
+    ///     Configures the bot application to use polling method for getting updates.
+    /// </summary>
+    /// <param name="app">The bot application builder.</param>
+    /// <param name="deleteWebhook">Indicates whether to delete the webhook on startup.</param>
+    /// <returns>A polling builder instance.</returns>
+    /// <exception cref="InvalidOperationException">Thrown if webhook is already enabled.</exception>
     public static IPollingBuilder UsePolling(this IBotApplicationBuilder app, bool deleteWebhook = false)
     {
         ArgumentNullException.ThrowIfNull(app);
@@ -25,6 +32,13 @@ public static class BotApplicationExtensions
         return pollingBuilder;
     }
 
+    /// <summary>
+    ///     Configures the bot application to use a webhook method for getting updates.
+    /// </summary>
+    /// <param name="app">The bot application builder.</param>
+    /// <param name="options">The webhook options.</param>
+    /// <returns>A webhook builder instance.</returns>
+    /// <exception cref="InvalidOperationException">Thrown if polling is already enabled.</exception>
     public static IWebhookBuilder UseWebhook(this IBotApplicationBuilder app, WebhookOptions options)
     {
         ArgumentNullException.ThrowIfNull(app);
@@ -42,6 +56,11 @@ public static class BotApplicationExtensions
         return webhookBuilder;
     }
 
+    /// <summary>
+    ///     Configures the bot to automatically answer callback queries.
+    /// </summary>
+    /// <param name="app">The bot application builder.</param>
+    /// <returns>The current instance of <see cref="IBotApplicationBuilder"/>.</returns>
     public static IBotApplicationBuilder UseCallbackAutoAnswering(this IBotApplicationBuilder app)
     {
         ArgumentNullException.ThrowIfNull(app);
