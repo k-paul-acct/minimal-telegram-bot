56b83a50fa6c7ce5fdf273d9c917e568771f1ef4
Pavel Kulakov <me@k-pavel.xyz>
2024-11-08 11:21:26 +0300

    new() constraint

--- src/StateMachine/Persistence/MinimalTelegramBot.StateMachine.Persistence.EntityFrameworkCore/IStateMachineDbContext.cs
+++ src/StateMachine/Persistence/MinimalTelegramBot.StateMachine.Persistence.EntityFrameworkCore/IStateMachineDbContext.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 namespace MinimalTelegramBot.StateMachine.Persistence.EntityFrameworkCore;
 
 public interface IStateMachineDbContext<TEntity>
-    where TEntity : class, IMinimalTelegramBotState
+    where TEntity : class, IMinimalTelegramBotState, new()
 {
     DbSet<TEntity> MinimalTelegramBotStates { get; }
 }
